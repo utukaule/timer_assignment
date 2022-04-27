b7c21d7703defc49f9998432aeae6a7c99427363
Utkatsh Kaule <utkarshkaule7@gmail.com>
2022-04-27 10:29:05 +0530

    Update Couter.jsx

--- timer/src/component/Couter.jsx
+++ timer/src/component/Couter.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import "../App.css"
 export const Counter = () => {
-
-    const [min, setMin] = useState(1);
+    const [minimum, setMin] = useState(1);
     const [sec, setSec] = useState(1);
 
     useEffect(() => {
@@ -12,38 +11,31 @@ export const Counter = () => {
                     setSec(sec + 1);
                 }
                 else {
-                    setMin(min + 1);
+                    setMin(minimum + 1);
 
                     setSec(sec * 0)
-
                 }
             }
             if (sec === 0) {
-                if (min === 0) {
+                if (minimum === 0) {
 
                     clearInterval(myId)
-
                 }
                 else {
 
-                    setMin(min + 1);
+                    setMin(minimum + 1);
                     setSec(sec + 1);
                 }
             }
         }, 1000);
         return () => {
-
             clearInterval(myId);
-
         }
     })
     return (
         <div>
-
             <h1 className="titel">Timer</h1>
-
-            <h1 className="count">{min}:{sec}</h1>
-
+            <h1 className="count">{minimum}:{sec}</h1>
         </div>
     )
 }
